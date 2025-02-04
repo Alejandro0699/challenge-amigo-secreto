9346ffa803171bd06029e7dcf89a9cf159e0239d
Alejandro0699 <alejandro.quintero26@hotmail.com>
2025-02-04 01:47:07 +0000

    creando la funcion almacenar amigos

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //comenzando con el challenge
+
+//creo la variable
+let listaDeNombres=[];
+//creo la funcion para alamcenar amigos
+function almacenarAmigos() {
+    
+    //creo una variable utlizanzando el DOM
+    let nombreAmigos=  document.getElementById("amigo")
+    nombreAmigos.innerHTML= "";
+
+    if (inputNombres) {
+
+        let amigo= inputNombres.value;
+
+        listaDeNombres.push
+
+        console.log(`lista de amigos : ${listaDeNombres}`);
+
+        inputNombres.value ="";
+        
+    } else {
+
+    }
+    
+    
+}
