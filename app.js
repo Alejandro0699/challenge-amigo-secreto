9a45b77cf10ae61e4ad723f4fea089f5a96d757d
Alejandro0699 <alejandro.quintero26@hotmail.com>
2025-02-06 00:23:24 +0000

    avanzamos en el codigo hasta verificar si el input esta vacio.

--- app.js
+++ app.js
@@ -6,23 +6,24 @@ let listaDeNombres=[];
 //creo la funcion para alamcenar amigos
 function almacenarAmigos() {
     
-    //creo una variable utlizanzando el DOM
-    let nombreAmigos=  document.getElementById("amigo")
-    nombreAmigos.innerHTML= "";
+    //creo una variable utlizanzando variables para obtener el input de los nombres y la lista de amigos donde se almacenan en la pantalla 
+    let inputNombres=  document.getElementById("amigo");
+    let listaAmigosUL= document.getElementById("listaAmigos");
+    //en el if utlizimos en boleanno "!" para verficiar que no existe el nombre en el input y luego lo retornamos
+    if (!inputNombres) {
+        console.error("no se encontro el input en el nombre");
+        return
+        }
 
-    if (inputNombres) {
+        //obtenemos el valor del input y elinamos los espacios en blanco (para eso se utliza el trim.)
+        let amigo= inputNombres.value.trim();
 
-        let amigo= inputNombres.value;
+        // validamos si el input esta vacio y luego lo retornamos 
+        listaDeNombres.push(amigo);
+        if (amigo === ""){
+            alert("Por favor ingrese un nombre valido.")
+            return;
+        }
 
-        listaDeNombres.push
-
-        console.log(`lista de amigos : ${listaDeNombres}`);
-
-        inputNombres.value ="";
-        
-    } else {
-
-    }
-    
     
 }
